aebf594a90e620301d56fca5448f703fa9dfd3c7
theofficialpeter <57006688+TheOfficialPeter@users.noreply.github.com>
2024-04-27 16:59:50 +0200

    Fixed SteamDB Scraping

--- main.py
+++ main.py
@@ -1,69 +1,84 @@
 import requests
 import json
 import cloudscraper
-from fastapi import FastAPI, Response, status
 from bs4 import BeautifulSoup
-from pydantic import BaseModel
-from typing import Dict, Any
+from fastapi import FastAPI
 
-app = FastAPI(
-    title="Game API",
-    description="API to fetch game data from ProtonDB and SteamDB"
-)
+app = FastAPI()
 
-class FetchResponse(BaseModel):
-    objectID: str
-    protondb_data: Dict[str, Any]
-    steamdb_data: Dict[str, Any]
 
-@app.get("/fetch", response_model=FetchResponse, responses={
-    200: {
-        "description": "Successful response",
-        "content": {
-            "application/json": {
-                "example": {
-                    "objectID": "123456",
-                    "protondb_data": {
-                        "confidence": "Strong",
-                        "score": 0.85,
-                        "total": 100,
-                        "tier": "Platinum"
-                    },
-                    "steamdb_data": {
-                        "status": "Success",
-                        "data": ["Item 1", "Item 2", "Item 3"],
-                        "status_text": "<html>...</html>"
-                    }
-                }
+def steamdb(number: str = ""):
+    """
+    Fetch data from SteamDB for a given game number.
+    """
+    print(f"Handling request to /steamdb with number: {number}")
+    if number:
+        url = f"https://steamdb.info/app/{number}"
+        try:
+            headers = {
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                "Accept-Language": "en-US,en;q=0.5",
+                "Alt-Used": "steamdb.info",
+                "Upgrade-Insecure-Requests": "1",
+                "Sec-Fetch-Dest": "document",
+                "Sec-Fetch-Mode": "navigate",
+                "Sec-Fetch-Site": "none",
+                "Sec-Fetch-User": "?1"
             }
-        }
-    },
-    400: {
-        "description": "Error response",
-        "content": {
-            "application/json": {
-                "example": "Could not find any results for: example_game"
+
+            cookies = {
+                "__cf_bm": "Bm.L70ZBVDPIBYT13DaZFXSI4rk6L8HFvLZX6uIuH4g-1714226892-1.0.1.1-8JGbMAD8hvFt1tLXTvcT_5IZdza3ZhRdgzUMz5VPVcUNTyx.AYq0tcHdoCMadDU4kvbuNoXsj948FaIXEdY2IQ",
+                "cf_clearance": "epYYau51_8uYdMRwRg4u_9H_RrohoBhKDpbm4O7cIQI-1714215584-1.0.1.1-sIywkIdwsTEqB2lysg7XjqiDw3idAaXP2aNZprd62WrZbFoqaLYhEyYtoQfpOAWt0.SfVZ0f3ryTwF0oSO6Y.w"
             }
-        }
-    }
-})
+
+            scraper = cloudscraper.create_scraper()
+            response = scraper.get(url, headers=headers, cookies=cookies)
+
+            if response.status_code == 200:
+                print("Received successful response from SteamDB")
+                soup = BeautifulSoup(response.text, 'html.parser')
+
+                # Find the div element with the selector 'div.header-thing-number:nth-child(1)'
+                header_thing_number = soup.select_one('div.header-thing-number:nth-child(1)')
+
+                if header_thing_number:
+                    text_value = header_thing_number.get_text(strip=True)
+                    return {"status": "Success", "data": [text_value]}
+                else:
+                    print(f"Could not find the 'header-thing-number' element on the page for number: {number}")
+                    return {"status": "Error", "message": f"Could not find the 'header-thing-number' element on the page for number: {number}"}
+            else:
+                print(f"Failed to fetch data from SteamDB for number: {number}")
+                return {"status": "Error", "message": f"Failed to fetch data from SteamDB for number: {number}, status code: {response.status_code}"}
+        except Exception as e:
+            print(f"Error fetching data from SteamDB for number: {number}")
+            return {"status": "Error", "message": f"Error fetching data from SteamDB for number: {number}, error: {str(e)}"}
+    else:
+        print("No number provided, returning a message")
+        return Response(status_code=status.HTTP_400_BAD_REQUEST, content="Please provide a number for the SteamDB request")
+
+@app.get("/")
+async def index():
+    print("Handling request to root URL (/)")
+    return {"message": "Hello World"}
+
+@app.get("/fetch")
 async def fetch(name: str = ""):
-    """
-    Fetch data from ProtonDB and SteamDB for a given game name.
-    """
     print(f"Handling request to /fetch with name: {name}")
-    payload = {
-        "query": "",
-        "attributesToHighlight": [],
-        "attributesToSnippet": [],
-        "facets": ["tags"],
-        "facetFilters": [["appType:Game"]],
-        "hitsPerPage": 50,
-        "attributesToRetrieve": ["lastUpdated", "name", "objectID", "followers", "oslist", "releaseYear", "tags", "technologies", "userScore"],
-        "page": 0
-    }
 
     if name:
+        payload = {
+            "query": "",
+            "attributesToHighlight": [],
+            "attributesToSnippet": [],
+            "facets": ["tags"],
+            "facetFilters": [["appType:Game"]],
+            "hitsPerPage": 50,
+            "attributesToRetrieve": ["lastUpdated", "name", "objectID", "followers", "oslist", "releaseYear", "tags", "technologies", "userScore"],
+            "page": 0
+        }
+
         print("Updating payload with the provided name")
         payload["query"] = name
         payload = json.dumps(payload)
@@ -99,46 +114,13 @@ async def fetch(name: str = ""):
                 # Call the steamdb function to fetch data from SteamDB
                 steamdb_data = steamdb(objectID)
 
-                return FetchResponse(objectID=objectID, protondb_data=extracted_data, steamdb_data=steamdb_data)
+                return {"objectID": objectID, "protondb_data": extracted_data, "steamdb_data": steamdb_data}
             else:
                 print(f"Failed to fetch data from ProtonDB for ObjectID: {objectID}")
-                return Response(status_code=status.HTTP_400_BAD_REQUEST, content=f"Failed to fetch data from ProtonDB for ObjectID: {objectID}")
+                return f"Failed to fetch data from ProtonDB for ObjectID: {objectID}"
         else:
             print(f"Could not find any results for: {name}")
-            return Response(status_code=status.HTTP_400_BAD_REQUEST, content=f"Could not find any results for: {name}")
+            return f"Could not find any results for: {name}"
     else:
         print("No name provided, returning a message")
-        return Response(status_code=status.HTTP_400_BAD_REQUEST, content="Please provide a name of a game to validate")
-
-def steamdb(number: str = ""):
-    """
-    Fetch data from SteamDB for a given game number.
-    """
-    print(f"Handling request to /steamdb with number: {number}")
-    if number:
-        url = f"https://steamdb.info/app/{number}"
-        try:
-            scraper = cloudscraper.create_scraper()
-            response = scraper.get(url)
-
-            if response.status_code == 200:
-                print("Received successful response from SteamDB")
-                soup = BeautifulSoup(response.text, 'html.parser')
-
-                # Find the ul element with the class 'app-chart-numbers-big'
-                app_chart_numbers = soup.find('ul', {'class': 'app-chart-numbers-big'})
-
-                # Extract the text values from the li elements
-                text_values = [li.get_text(strip=True) for li in app_chart_numbers.find_all('li')]
-
-                return {"status": "Success", "data": text_values, "status_text": response.text}
-            else:
-                print(f"Failed to fetch data from SteamDB for number: {number}")
-                return {"status": "Error", "message": f"Failed to fetch data from SteamDB for number: {number}, status code: {response.status_code}", "status_text": response.text}
-        except Exception as e:
-            print(f"Error fetching data from SteamDB for number: {number}")
-            return {"status": "Error", "message": f"Error fetching data from SteamDB for number: {number}, error: {str(e)}", "status_text": ""}
-    else:
-        print("No number provided, returning a message")
-        return Response(status_code=status.HTTP_400_BAD_REQUEST, content="Please provide a number for the SteamDB request")
-
+        return "Please provide a name of a game to validate"
