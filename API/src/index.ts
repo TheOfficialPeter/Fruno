55fef53b76159aa0579531d1a1afab52f3a1b97e
theofficialpeter <57006688+TheOfficialPeter@users.noreply.github.com>
2024-05-11 15:27:10 +0200

    [+] Cleaned code + Added Media function ( Not working on Cloudflare Workers )

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,5 +1,3 @@
-// @ts-nocheck
-
 import { Hono } from 'hono'
 import { HTTPException } from 'hono/http-exception'
 import { cors } from 'hono/cors'
@@ -8,11 +6,24 @@ const app = new Hono()
 app.use('*', cors())
 
 async function validate(gameName: string): Promise<[boolean, string, any, any]> {
-  if ( gameName != null && gameName != "" && gameName != " " ) {
-    var protonResponse: any = await fetch("https://94he6yatei-dsn.algolia.net/1/indexes/steamdb/query", {
+  if (!gameName || gameName.trim() === "") {
+    return [false, 'Game name was not provided. Please try again', null, null];
+  }
+
+  try {
+    const protonResponse: any = await fetch("https://94he6yatei-dsn.algolia.net/1/indexes/steamdb/query", {
       method: "POST",
-      body: JSON.stringify({"query":gameName,"attributesToHighlight":[],"attributesToSnippet":[],"facets":["tags"],"facetFilters":[["appType:Game"]],"hitsPerPage":50,"attributesToRetrieve":["lastUpdated","name","objectID","followers","oslist","releaseYear","tags","technologies","userScore"],"page":0}),
-      headers: { 
+      body: JSON.stringify({
+        "query": gameName,
+        "attributesToHighlight": [],
+        "attributesToSnippet": [],
+        "facets": ["tags"],
+        "facetFilters": [["appType:Game"]],
+        "hitsPerPage": 50,
+        "attributesToRetrieve": ["lastUpdated", "name", "objectID", "followers", "oslist", "releaseYear", "tags", "technologies", "userScore"],
+        "page": 0
+      }),
+      headers: {
         "Content-Type": "application/json",
         "x-algolia-api-key": "9ba0e69fb2974316cdaec8f5f257088f",
         "x-algolia-application-id": "94HE6YATEI",
@@ -20,41 +31,23 @@ async function validate(gameName: string): Promise<[boolean, string, any, any]> 
       }
     });
 
-    if ( !protonResponse.ok ) {
+    if (!protonResponse.ok) {
       return [false, 'Request to ProtonDB failed. Status did not return OK', null, null];
     }
 
-    try {
-      protonResponse = await protonResponse.json();
-    }
-    catch (err) {
-      return [false, 'Request to ProtonDB failed. Could not convert to JSON', null, null];
-    }
+    const protonResponseData = await protonResponse.json();
 
-    if ( 'hits' in protonResponse && protonResponse['hits'].length > 0) {
-      var protonTierResponse: any = await fetch(`https://www.protondb.com/api/v1/reports/summaries/${ protonResponse['hits'][0]['objectID'] }.json`)
-
-      try {
-        protonTierResponse = await protonTierResponse.json();
-      }
-      catch (err) {
-        return [false, 'Request to ProtonDB Game Info failed. Could not convert to JSON', null, null];
-      }
-
-      return [true, 'Request to ProtonDB succeeded', protonResponse, protonTierResponse];
-    }
-    else if ( 'hits' in protonResponse )
-    {
+    if ('hits' in protonResponseData && protonResponseData['hits'].length > 0) {
+      const protonTierResponse = await fetch(`https://www.protondb.com/api/v1/reports/summaries/${protonResponseData['hits'][0]['objectID']}.json`);
+      const protonTierResponseData = await protonTierResponse.json();
+      return [true, 'Request to ProtonDB succeeded', protonResponseData, protonTierResponseData];
+    } else if ('hits' in protonResponseData) {
       return [false, 'Game not found', null, null];
-    }
-    else
-    {
+    } else {
       return [false, 'It seems that ProtonDB might have changed their API. Please wait for an update', null, null];
     }
-  }
-  else
-  {
-    return [false, 'Game name was not provided. Please try again', null, null];
+  } catch (err) {
+    return [false, 'Request to ProtonDB failed. Could not convert to JSON', null, null];
   }
 }
 
@@ -70,28 +63,67 @@ function extractValues(protonResponse: any, protonTier: any): [boolean, string, 
   }
 }
 
+async function getSteamMedia(gameId: number): Promise<[boolean, string, any]> {
+  try {
+    let response: any = await fetch("https://www.protondb.com/proxy/steam/api/appdetails/?appids=" + gameId.toString(), {
+      headers: {
+          "referrer": "https://www.protondb.com/app/" + gameId.toString(),
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0",
+          "Accept": "*/*",
+          "Accept-Language": "en-US,en;q=0.5",
+          "Sec-Fetch-Dest": "empty",
+          "Sec-Fetch-Mode": "cors",
+          "Sec-Fetch-Site": "same-origin",
+          "mode": "cors",
+      },
+      method: "GET",
+    });
+
+    response = await response.json();
+
+    const gameImage = response[gameId]['data']['header_image'];
+    const gameTrailer = response[gameId]['data']['movies'][0]['mp4']['480'];
+    return [true, 'Media fetched successfully', {image: gameImage, trailer: gameTrailer}];
+  } catch (err) {
+    return [false, 'Request to Steam API failed. Could not convert to JSON', null];
+  }
+}
+
 app.get('/', (c) => {
   throw new HTTPException(404, {message: 'Route Not Found. Did you perhaps mean /fetch?'})
 })
 
 app.get('/fetch', async (c, next) => {
-  var [success, reason, results, tier] = await validate(c.req.query('name') as string);
+  try {
+    // -- Validate
+    const [success, reason, results, tier] = await validate(c.req.query('name') as string);
 
-  if ( success ) {
-    [success, reason, results] = extractValues(results, tier);
+    if (!success) {
+      return c.text(`Validation Failed. Reason: ${reason}`, 500);
+    }
 
-    if ( success ) {
-      return c.json(results);
+    // -- Extract
+    const [extractSuccess, extractReason, extractedValues] = extractValues(results, tier);
+
+    if (!extractSuccess) {
+      return c.text(`Something went wrong when extracting values from ProtonDB. Reason: ${extractReason}`, 500);
     }
-    else
-    {
-      return c.text(`Something went wrong when extracting values from ProtonDB. Reason: ${reason}`, 500);
+
+    // -- Media
+    const [mediaSuccess, mediaReason, media] = await getSteamMedia(extractedValues.gameId);
+
+    if (!mediaSuccess) {
+      return c.text(`Error occurred while fetching media from Steam. Reason: ${mediaReason}`, 500);
     }
+
+    // -- Return
+    extractedValues.media = media;
+    return c.json(extractedValues);
+  } catch (error: any) {
+    return c.text(`Something went wrong. Reason: ${error.message}`, 500);
   }
-  else
-  {
-    return c.text(`Something went wrong. Reason: ${reason}`, 500);
-  }
-})
+});
 
 export default app
+
+
