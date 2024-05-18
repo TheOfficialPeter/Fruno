47b6ce5c97a1ab97da6c43813c29f8809729fd21
theofficialpeter <57006688+TheOfficialPeter@users.noreply.github.com>
2024-05-18 23:07:53 +0200

    [+] Cleaned code + Added validation Basic

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,24 +1,26 @@
 import { Hono } from 'hono'
 import { HTTPException } from 'hono/http-exception'
-import * as cheerio from 'cheerio';
 
 const app = new Hono()
 
 async function validateGame(gameName: string): Promise<[boolean, string, any, any]> {
-  const payload = {"query": gameName,"attributesToHighlight":[],"attributesToSnippet":[],"facets":["tags"],"facetFilters":[["appType:Game"]],"hitsPerPage":100,"attributesToRetrieve":["lastUpdated","name","objectID","followers","oslist","releaseYear","tags","technologies","userScore"],"filters":"(objectID:567380)"}
+  const payload = {"query": gameName,"attributesToHighlight":[],"attributesToSnippet":[],"facets":["tags"],"facetFilters":[["appType:Game"]],"hitsPerPage":100,"attributesToRetrieve":["lastUpdated","name","objectID","followers","oslist","releaseYear","tags","technologies","userScore"]}
   let response: any = await fetch("https://94he6yatei-dsn.algolia.net/1/indexes/steamdb/query", {
     method: "POST",
     headers: {
       'x-algolia-api-key': '9ba0e69fb2974316cdaec8f5f257088f',
-      'x-algolia-application-id': '94HE6YATEI'
+      'x-algolia-application-id': '94HE6YATEI',
+      'Referer': 'https://www.protondb.com/'
     },
     body: JSON.stringify(payload)
   })
 
+  console.log(response);
+
   if (response.ok) {
     response = await response.json();
     if (response['hits'].length > 0) {
-      return [true, 'Game Found', null, null];
+      return [true, 'Game Found', response['hits'][0].name, null];
     }
     else
     {
@@ -51,12 +53,12 @@ app.get('/', (c) => {
 })
 
 app.get('/fetch', async(c) => {
-  const gameName = c.req.param('name');
+  const gameName = c.req.query('name');
   if (gameName != undefined) {
     const validateResponse = await validateGame(gameName);
 
     if (validateResponse[0]) {
-      return c.text(gameName + " was found in the games list")
+      return c.text(validateResponse[2] + " was found in the games list")
     }
     else
     {
