8f352b45834362d0520a979590a588150465000d
theofficialpeter <57006688+TheOfficialPeter@users.noreply.github.com>
2024-08-02 21:58:19 +0200

    👌 Optimizations and Cleanup

--- Enums/enums.py
+++ Enums/enums.py
@@ -1,14 +1,8 @@
 from enum import Enum
 
-"""
-File Info:
-File contains classes with Enum values used for increased readability
-"""
-
 class Role(Enum):
     ADMIN='Developer'
     MODERATOR='Mod'
     BUYER='Buyer'
     VERIFIED='Verified'
     TRUSTED='Trusted'
-    
--- Logic/Process/processGameInfo.py
+++ Logic/Process/processGameInfo.py
@@ -3,20 +3,6 @@ def process_game_info(apiResponse):
         try:
             apiResponse = apiResponse.json()
 
-            match apiResponse['tier'].lower():
-                case "bronze":
-                    apiResponse['desc'] = ' (Game has significant compatibility issues on Linux with Proton)'
-                case "silver":
-                    apiResponse['desc'] = ' (Game has minor compatibility problems on Linux with Proton)'
-                case "gold":
-                    apiResponse['desc'] = ' (Game runs very well on Linux with Proton)'
-                case "platinum":
-                    apiResponse['desc'] = ' (Game runs flawlessly on Linux with Proton)'
-                case "borked":
-                    apiResponse['desc'] = ' (Game is completely broken and unplayable on Linux with Proton)'
-                case _:
-                    apiResponse['desc'] = ' (Invalid ProtonDB tier. Linux game compatibility is unknown)'
-
             return [True, "Game information fetched successfully", apiResponse]
         except Exception as e:
             print("[ERROR] Failed to parse JSON response for Fetch Command: ", str(e))
